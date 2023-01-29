90fba64cd13f41b32393d8c6a0f9183402930030
Ayyasudeen <63138561+Ayyasudeen@users.noreply.github.com>
2023-01-29 13:59:29 +0530

    added transaction description section

--- src/App.js
+++ src/App.js
@@ -16,6 +16,38 @@ function App() {
         </div>
         <button type="submit">Add New Transaction</button>
       </form>
+      <div className="transactions">
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Samsung Tv</div>
+            <div className="description">It was time for new TV</div>
+          </div>
+          <div className="right">
+            <div className="price">$500</div>
+            <div className="datetime">2023-01-29 13:58</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Samsung Tv</div>
+            <div className="description">It was time for new TV</div>
+          </div>
+          <div className="right">
+            <div className="price">$500</div>
+            <div className="datetime">2023-01-29 13:58</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Samsung Tv</div>
+            <div className="description">It was time for new TV</div>
+          </div>
+          <div className="right">
+            <div className="price">$500</div>
+            <div className="datetime">2023-01-29 13:58</div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
