bde023c819358e89194bac4e6c3939bd635ed72f
Ayyasudeen <63138561+Ayyasudeen@users.noreply.github.com>
2023-02-13 21:16:19 +0530

    created api endpoints in the backend

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors')
 const app = express()
 const port = 4000
 
-app.get('/api/test', (req, res) => {
+app.use(cors());
+app.use(express.json())
+app.get('/api_v1/test', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/api_v1/transaction', (req, res) => {
+    res.json(req.body)
+  })
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
