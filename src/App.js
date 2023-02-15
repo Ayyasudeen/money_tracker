cf4676f5627c16eb734735c419f1d0a95dc3bd8c
Ayyasudeen <63138561+Ayyasudeen@users.noreply.github.com>
2023-02-15 22:58:06 +0530

    adding extra fields and features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [name, setName] = useState("")
   const [datetime, setDatetime] = useState("")
   const [description, setDescription] = useState("")
+  const [moneyValue, setMoneyValue] = useState("")
+  const [transactionsData, setTransactionsData] = useState([])
 
   // takes all the data and sends it to the backend
   function addNewTransaction(e) {
@@ -22,6 +24,26 @@ function App() {
         console.log('Success:', data);
       })
   }
+
+  function getAllTransactions() {
+    const url = process.env.REACT_APP_API_URL + "/transactions";
+
+  fetch(url, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+  })
+      .then(response => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+        setTransactionsData(data)
+      })
+  }
+
+  useEffect(() => {
+    getAllTransactions()
+  }, [])
+  
+
   return (
     <main>
       <h1>
@@ -29,11 +51,18 @@ function App() {
       </h1>
       <form onSubmit={addNewTransaction}>
         <div className="basic">
-          <input type="text" value={name} onChange={e=> setName(e.target.value)} placeholder="+$200 Samsung TV" />
+          <input type="text" value={name} onChange={e=> setName(e.target.value)} placeholder="Title" />
           <input type="datetime-local" value={datetime} onChange={e=> setDatetime(e.target.value)}/>
         </div>
         <div className="description">
-          <input type="text" value={description} onChange={e=> setDescription(e.target.value)} placeholder="description" />
+          <input type="text" value={description} onChange={e=> setDescription(e.target.value)} placeholder="Description" />
+          <select>
+            <option value={1}>Expense</option>
+            <option value={0}>Income</option>
+          </select>
+        </div>
+        <div className="description">
+          <input type="number" value={moneyValue} onChange={e=> setMoneyValue(e.target.value)} placeholder="$" />
         </div>
         <button type="submit">Add New Transaction</button>
       </form>
